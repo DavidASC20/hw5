002a0c7fde584e6dad0441be631a812989ef61b3
DavidASC20 <djchong24@gmail.com>
2021-09-29 23:50:14 -0400

    finished step 4

--- hw5.c
+++ hw5.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char c = 'c';
-char a = 'a';
-int x = 5;
-int y = 17;
-long i = 92233720368547742;
-
 
 int main(){
-    printf("Hello World");
+    char c = 'c';
+    int i = 5;
+    long l = 92233720368547742;
+
+    printf("Variable is %c\nHex: %p\nDecimal:%lu\n", c, &c, &c);
+    printf("Variable is %i\nHex: %p\nDecimal:%lu\n", i, &i, &i);
+    printf("Variable is long\nHex: %p\nDecimal:%lu\n", l, &l, &l);
+
+    char *pointer_c = &c;
+    int *pointer_i = &i;
+    long *pointer_l = &l;
+
+    printf("char pointer: %p\nint pointer: %p\nlong pointer: %p\n", pointer_c, pointer_i, pointer_l);
+
 }
